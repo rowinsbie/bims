c0f665aa5aca8ab7bc7b6c4d2bb609b165645c0d
Rowins Bie <rowins.bie.srb@gmail.com>
2022-11-15 08:00:38 +0800

    setup scss and front-end

--- bims-FE/src/App.vue
+++ bims-FE/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import NavigationBarVue from "./components/NavigationBar.vue";
 </script>
 
 <template>
+    <NavigationBarVue />
     <RouterView />
 </template>
 
--- bims-FE/src/components/NavigationBar.vue
+++ bims-FE/src/components/NavigationBar.vue
@@ -3,7 +3,37 @@
 </script>
 
 <template>
-    <div>
-        <h1>Test</h1>
-    </div>
+    <nav>
+        <h1>nav</h1>
+        <ul class="nav-group">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+            <li>
+                <a href="">Officials</a>
+            </li>
+        </ul>
+    </nav>
 </template>
+<style lang="scss">
+    nav {
+        display: flex;
+        justify-content: space-around;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        color: white;
+        background-color: black;
+        padding: 1rem 1rem;
+        .nav-group {
+            display: flex;
+            li {
+                padding: 0 1rem;
+            }
+        }
+    }
+</style>
