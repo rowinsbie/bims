f0b65f8e8a16b4d51dab275a7543404a4e2290f0
Rowins Bie <rowins.bie.srb@gmail.com>
2022-11-29 21:37:32 +0800

    created a post form component

--- bims-FE/src/components/social_media/PostForm.vue
+++ bims-FE/src/components/social_media/PostForm.vue
@@ -1,17 +1,23 @@
 <template>
 <div>
-    <textarea  rows="3" class="form-control sulatan" placeholder="Ano ang iyong gustong sabihin?"></textarea>
+    <textarea  rows="3" id="sulatan" class="form-control " placeholder="Express your thoughts"></textarea>
     <hr />
-    <ul class="actions">
-        <li>
-            <font-awesome-icon class="action-buttons" icon="fa-solid fa-user" />
-        </li>
-        <li>
-            <font-awesome-icon class="action-buttons" icon="fa-solid fa-file-image" />                            </li>
-        <li>
-            <button class="btn btn-primary"> <font-awesome-icon icon="fa-solid fa-paper-plane" /> Ibahagi</button>
-        </li>
-    </ul>
+    <div class="post-tools">
+        <ul class="actions">
+            <li>
+                <font-awesome-icon class="action-buttons" icon="fa-solid fa-user" />
+            </li>
+            <li>
+                <font-awesome-icon class="action-buttons" icon="fa-solid fa-file-image" />                            </li>
+          
+        </ul>
+        <ul class="actions">
+         
+            <li>
+                <button class="btn btn-primary"> <font-awesome-icon icon="fa-solid fa-paper-plane" /> Express</button>
+            </li>
+        </ul>
+    </div>
 </div>
 </template>
 <script lang="ts">
@@ -22,26 +28,36 @@ export default defineComponent({
 });
 </script>
 <style lang="scss">
-.sulatan {
-    border-width: 0px IM !important;
-    font-size: 30px;   
+#sulatan {
+   border: 1px solid rgb(241, 241, 241);
+    font-size: 20px;   
+    color: rgb(56, 56, 56);
 
 }
 
-.actions {
+#sulatan:focus {
+    outline: none;
+}
+
+.post-tools {
     display: flex;
-    gap: 16px;
-    justify-content: end;
-    align-items: flex-end;
-    .action-buttons {
-        font-size: 2rem;
-        color: rgb(88, 164, 235);
-    }
-    li:last-child {
-        button {
-            border-radius: 10px 10px 10px 10px;
+    justify-content: space-between;
+    align-items: center;
+    .actions {
+        display: flex;
+        gap: 16px;
+        justify-content: end;
+        align-items: flex-end;
+        .action-buttons {
+            font-size: 2rem;
+            color: rgb(88, 164, 235);
         }
+        li:last-child {
+            button {
+                border-radius: 10px 10px 10px 10px;
+            }
+        }
+       
     }
-   
 }
 </style>
