9e3200ebbf2051adc9cde2c03fcedf37ee45a2c1
Rowins Bie <rowins.bie.srb@gmail.com>
2023-02-02 21:19:00 +0800

    update

---
+++ bims-FE/src/components/cards/cardStatistic.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import Test from '@/assets/images/story-set/group.png';
+</script>
+
+<template>
+   <div class="card">
+        <div class="card-body">
+            <div class="header-card">
+                <img src="../../assets/images/story-set/group.png" width="100" />
+                 <div class="card-text">
+                        <span class="color-label">Residents</span>
+                         <h1>1,459</h1>
+                 </div>
+            </div>
+                         
+        </div>
+   </div>
+</template>
+
+<style lang="scss">
+.icon {
+    background:red;
+    padding:0.7rem;
+    border-radius:50%;
+    color:white;
+}
+.header-card {
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+}
+</style>
--- bims-FE/src/views/auth/DashboardView.vue
+++ bims-FE/src/views/auth/DashboardView.vue
@@ -3,13 +3,16 @@ import  { defineComponent } from 'vue';
 import http from '../../http/Http';
 import UserPicture from '@/components/kits/CircleImage.vue';
 import DefaultPicture from '../../assets/images/user/user-picture.jpg';
-
+import cardStatistic from '@/components/cards/cardStatistic.vue';
+import LayOut from "@/components/layout/LayOut.vue";
 // data types
 import type {User} from '../../types/index';
 
 export default defineComponent({
     components:{
         UserPicture,
+        LayOut,
+        cardStatistic
     },
     data() {
         return {
@@ -34,12 +37,17 @@ export default defineComponent({
 })
 </script>
 <template>
-    <div class="container mt-5">
-        <div class="row mt-5">
-           
+    <LayOut>
+
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-3 ">
+                <cardStatistic />
+            </div>
 
         </div>
     </div>
+    </LayOut>
 </template>
 <style lang="scss">
 
